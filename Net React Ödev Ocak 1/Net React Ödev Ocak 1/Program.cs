1f48aa0220eb2b4f8782e2fdf28d54dfcf0baa0b
Sefa <yusufsefayetkin@gmail.com>
2025-03-05 01:17:38 +0300

    Ocak 1
    Net React Ödev - Ocak 1

---
+++ Net React Ödev Ocak 1/Net React Ödev Ocak 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Kullanıcıdan adını al
+        Console.Write("Adınızı girin: ");
+        string ad = Console.ReadLine();
+
+        // Kullanıcının adını ekrana yazdır
+        Console.WriteLine("Merhaba, " + ad + "!");
+    }
+}
